11b79d1a7dd6518c36527b572fdecad10ef24080
suryaoke <89033805+suryaoke@users.noreply.github.com>
2023-09-02 11:31:53 +0700

    Add files via upload

---
+++ sanbox_.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Sep 2023 pada 06.30
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sanbox_`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `asalsurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.asalsurat: #1932 - Table 'sanbox_.asalsurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.asalsurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`asalsurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `contoh`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.contoh: #1932 - Table 'sanbox_.contoh' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.contoh: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`contoh`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `ijazah`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.ijazah: #1932 - Table 'sanbox_.ijazah' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.ijazah: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`ijazah`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `informasisurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.informasisurat: #1932 - Table 'sanbox_.informasisurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.informasisurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`informasisurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `isisurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.isisurat: #1932 - Table 'sanbox_.isisurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.isisurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`isisurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jabatan`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.jabatan: #1932 - Table 'sanbox_.jabatan' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.jabatan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`jabatan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lampiransurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.lampiransurat: #1932 - Table 'sanbox_.lampiransurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.lampiransurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`lampiransurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `log`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.log: #1932 - Table 'sanbox_.log' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`log`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migration`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.migration: #1932 - Table 'sanbox_.migration' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.migration: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`migration`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `naskahdinas`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.naskahdinas: #1932 - Table 'sanbox_.naskahdinas' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.naskahdinas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`naskahdinas`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notif1`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.notif1: #1932 - Table 'sanbox_.notif1' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.notif1: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`notif1`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notif2`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.notif2: #1932 - Table 'sanbox_.notif2' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.notif2: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`notif2`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembuatsurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.pembuatsurat: #1932 - Table 'sanbox_.pembuatsurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.pembuatsurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`pembuatsurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `role`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.role: #1932 - Table 'sanbox_.role' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.role: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`role`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `smdisposisi`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.smdisposisi: #1932 - Table 'sanbox_.smdisposisi' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.smdisposisi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`smdisposisi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `smpenerima`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.smpenerima: #1932 - Table 'sanbox_.smpenerima' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.smpenerima: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`smpenerima`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `smterkirim`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.smterkirim: #1932 - Table 'sanbox_.smterkirim' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.smterkirim: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`smterkirim`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `suratmasuk`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.suratmasuk: #1932 - Table 'sanbox_.suratmasuk' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.suratmasuk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`suratmasuk`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tandatangan`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.tandatangan: #1932 - Table 'sanbox_.tandatangan' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.tandatangan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`tandatangan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tembusansurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.tembusansurat: #1932 - Table 'sanbox_.tembusansurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.tembusansurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`tembusansurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tujuansurat`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.tujuansurat: #1932 - Table 'sanbox_.tujuansurat' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.tujuansurat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`tujuansurat`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.user: #1932 - Table 'sanbox_.user' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`user`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `verifikasi`
+--
+-- Kesalahan membaca struktur untuk tabel sanbox_.verifikasi: #1932 - Table 'sanbox_.verifikasi' doesn't exist in engine
+-- Kesalahan membaca data untuk tabel sanbox_.verifikasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sanbox_`.`verifikasi`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
